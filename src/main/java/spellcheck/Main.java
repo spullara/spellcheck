fc03ed48c2777e950244125d696f4a9cdab3b890
Sam Pullara <spullara@wavefront.com>
2014-12-31 15:41:37 -0800

    change this around to see if i can see a better way

--- src/main/java/spellcheck/Main.java
+++ src/main/java/spellcheck/Main.java
@@ -1,13 +1,18 @@
 package spellcheck;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Supplier;
-import java.util.stream.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static java.util.stream.Stream.concat;
 
 public class Main {
@@ -16,19 +21,39 @@ public class Main {
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
     List<String[]> splits = IntStream.range(0, word.length())
             .mapToObj(i -> new String[]{word.substring(0, i), word.substring(i)}).collect(toList());
-    return concat(splits.stream().filter(p -> p[1].length() > 1).map(p -> p[0] + p[1].substring(1)), // deletes
-            concat(splits.stream().filter(p -> p[1].length() > 2)
-                            .map(p -> p[0] + p[1].substring(1, 2) + p[1].substring(0, 1) + p[1].substring(2)), // inserts
-                    concat(splits.stream().filter(p -> p[1].length() > 1)
-                                    .flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1].substring(1))), // transposes
-                            splits.stream().flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1]))))); //inserts
+    Stream<String> deletes = splits.stream().filter(p -> p[1].length() > 1).map(p -> p[0] + p[1].substring(1));
+    Stream<String> transposes = splits.stream().filter(p -> p[1].length() > 2)
+            .map(p -> p[0] + p[1].substring(1, 2) + p[1].substring(0, 1) + p[1].substring(2));
+    Stream<String> replaces = splits.stream().filter(p -> p[1].length() > 1)
+            .flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1].substring(1)));
+    Stream<String> inserts = splits.stream().flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1]));
+    return concat(deletes, concat(transposes, concat(replaces, inserts)));
   }
 
-  static Stream<String> edits(String word) { return edit(word).flatMap(Main::edit); }
+  static Stream<String> edit2(String word) {
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    List<String[]> splits = IntStream.range(0, word.length())
+            .mapToObj(i -> new String[]{word.substring(0, i), word.substring(i)}).collect(toList());
+    Stream<String> deletes = splits.stream().filter(p -> p[1].length() > 1).map(p -> p[0] + p[1].substring(1));
+    Stream<String> transposes = splits.stream().filter(p -> p[1].length() > 2)
+            .map(p -> p[0] + p[1].substring(1, 2) + p[1].substring(0, 1) + p[1].substring(2));
+    Stream<String> replaces = splits.stream().filter(p -> p[1].length() > 1)
+            .flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1].substring(1)));
+    Stream<String> inserts = splits.stream().flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1]));
+    return concat(deletes, concat(transposes, concat(replaces, inserts)));
+  }
 
-  static Set<String> known(Stream<String> words) { return words.filter(NWORDS::contains).collect(toSet()); }
+  static Stream<String> edits(String word) {
+    return edit(word).flatMap(Main::edit);
+  }
 
-  static <T extends Collection<V>, V> T or(T f, Supplier<T> s) { return f.isEmpty() ? s.get() : f; }
+  static Set<String> known(Stream<String> words) {
+    return words.filter(NWORDS::contains).collect(toSet());
+  }
+
+  static <T extends Collection<V>, V> T or(T f, Supplier<T> s) {
+    return f.isEmpty() ? s.get() : f;
+  }
 
   static Collection<String> correct(String word) {
     return or(known(Stream.of(word)),
