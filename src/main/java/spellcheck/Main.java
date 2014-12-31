78d801b469ae286d87ea873b3d4a1b4c17c1a380
Sam Pullara <spullara@wavefront.com>
2014-12-31 14:57:52 -0800

    some more refactoring and cleanup

--- src/main/java/spellcheck/Main.java
+++ src/main/java/spellcheck/Main.java
@@ -12,9 +12,8 @@ import static java.util.stream.Stream.concat;
 
 public class Main {
 
-  static String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-  static Stream<String> edits(String word) {
+  static Stream<String> edit(String word) {
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
     List<String[]> splits = IntStream.range(0, word.length())
             .mapToObj(i -> new String[]{word.substring(0, i), word.substring(i)}).collect(toList());
     return concat(splits.stream().filter(p -> p[1].length() > 1).map(p -> p[0] + p[1].substring(1)), // deletes
@@ -25,28 +24,26 @@ public class Main {
                             splits.stream().flatMap(p -> alphabet.chars().mapToObj(c -> p[0] + (char) c + p[1]))))); //inserts
   }
 
-  static Stream<String> edits2(String word) { return edits(word).flatMap(Main::edits); }
+  static Stream<String> edits(String word) { return edit(word).flatMap(Main::edit); }
 
-  static Set<String> known(Stream<String> words) { return words.filter(NWORDS::containsKey).collect(toSet()); }
+  static Set<String> known(Stream<String> words) { return words.filter(NWORDS::contains).collect(toSet()); }
 
-  static <T extends Collection<V>, V> T or(T first, Supplier<T> second) { return first.isEmpty() ? second.get() : first; }
+  static <T extends Collection<V>, V> T or(T f, Supplier<T> s) { return f.isEmpty() ? s.get() : f; }
 
   static Collection<String> correct(String word) {
-    Set<String> suggestions = new TreeSet<>((s1, s2) -> s1.length() - s2.length());
-    suggestions.addAll(or(known(Stream.of(word)),
-            () -> or(known(edits(word)),
-                    () -> or(known(edits2(word)),
-                            () -> asList(word)))));
-    return suggestions;
+    return or(known(Stream.of(word)),
+            () -> or(known(edit(word)),
+                    () -> or(known(edits(word)),
+                            () -> asList(word))));
   }
 
-  static Map<String, Integer> NWORDS;
+  static Set<String> NWORDS;
 
   public static void main(String[] args) throws IOException {
     NWORDS = Files.lines(new File(args[0]).toPath())
             .map(String::toLowerCase)
             .filter((s) -> s.matches("[a-z]+"))
-            .collect(toMap(s -> s, s -> 1, (s1, s2) -> 1));
+            .collect(toSet());
 
     String line;
     while ((line = System.console().readLine()) != null) {
