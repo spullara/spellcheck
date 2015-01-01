35ccc13a1aed045b5459fbb0d0bd3cdfeb52e919
Sam Pullara <spullara@wavefront.com>
2014-12-31 16:34:52 -0800

    add the sorting by corpus commonality back in

--- src/main/java/spellcheck/Main.java
+++ src/main/java/spellcheck/Main.java
@@ -4,56 +4,55 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toSet;
+import static java.util.Arrays.copyOfRange;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.range;
-import static java.util.stream.Stream.concat;
 
 public class Main {
 
   static Stream<String> edit(String w) {
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
     int len = w.length();
-    Stream<String> deletes = range(0, len - 1).mapToObj(i -> w.substring(0, i) + w.substring(i + 1));
-    Stream<String> transposes = range(0, len - 2)
+    Stream<String> deletes = range(0, len).mapToObj(i -> w.substring(0, i) + w.substring(i + 1));
+    Stream<String> transposes = range(0, len - 1)
             .mapToObj(i -> w.substring(0, i) + w.substring(i + 1, i + 2) + w.substring(i, i + 1) + w.substring(i + 2));
-    Stream<String> replaces = range(0, len - 1).boxed()
+    Stream<String> replaces = range(0, len).boxed()
             .flatMap(i -> alphabet.chars().mapToObj(c -> w.substring(0, i) + (char) c + w.substring(i + 1)));
     Stream<String> inserts = range(0, len).boxed()
             .flatMap(i -> alphabet.chars().mapToObj(c -> w.substring(0, i) + (char) c + w.substring(i)));
-    return concat(deletes, concat(transposes, concat(replaces, inserts)));
+    return Stream.of(deletes, transposes, replaces, inserts).flatMap(s -> s);
   }
 
   static Stream<String> edits(String word) {
     return edit(word).flatMap(Main::edit);
   }
 
-  static Set<String> known(Stream<String> words) {
-    return words.filter(NWORDS::contains).collect(toSet());
+  static List<String> known(Stream<String> words) {
+    return words.filter(NWORDS::containsKey).sorted((s1, s2) -> NWORDS.get(s2) - NWORDS.get(s1)).collect(toList());
   }
 
-  static <T extends Collection<V>, V> T or(T f, Supplier<T> s) {
-    return f.isEmpty() ? s.get() : f;
+  @SafeVarargs static <T extends Collection<V>, V> T or(T f, Supplier<T>... s) {
+    return f.isEmpty() ? s.length == 1 ? s[0].get() : or(s[0].get(), copyOfRange(s, 1, s.length)) : f;
   }
 
   static Collection<String> correct(String word) {
-    return or(known(Stream.of(word)),
-            () -> or(known(edit(word)),
-                    () -> or(known(edits(word)),
-                            () -> asList(word))));
+    return or(known(Stream.of(word)), () -> known(edit(word)), () -> known(edits(word)), () -> asList(word));
   }
 
-  static Set<String> NWORDS;
+  static Map<String, Integer> NWORDS;
 
   public static void main(String[] args) throws IOException {
     NWORDS = Files.lines(new File(args[0]).toPath())
             .map(String::toLowerCase)
-            .filter((s) -> s.matches("[a-z]+"))
-            .collect(toSet());
+            .filter(s -> s.matches("[a-z]+"))
+            .collect(toMap(s -> s, s -> 1, (v1, v2) -> v1 + v2));
 
     String line;
     while ((line = System.console().readLine()) != null) {
